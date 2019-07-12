87b7d62b60e2d4e9d4e706d24b3d16dbd50b0c1c
Harminder virk <virk.officials@gmail.com>
2019-07-12 13:49:11 +0530

    docs: update readme and generate api docs

---
+++ index.ts
@@ -0,0 +1,17 @@
+/*
+ * @poppinss/hash
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+export { Hash } from './src/Hash'
+export {
+  BcryptConfigContract,
+  ArgonConfigContract,
+  HashConfigContract,
+  HashDrivers,
+  HashDriverContract,
+} from './src/contracts'
