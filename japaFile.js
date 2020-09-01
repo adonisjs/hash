7ab805c39a579ecaf60565c6b0380f37ba1b14f6
Evgeniy <cmp08@ya.ru>
2020-09-01 19:26:41 +0300

    feat: run single test feature

--- japaFile.js
+++ japaFile.js
@@ -1,8 +1,21 @@
+const { join, isAbsolute, sep } = require('path')
 require('ts-node').register({
 	files: true,
 })
 
+function getTestFiles() {
+	let userDefined = process.argv.slice(2)[0]
+	if (!userDefined) {
+		return 'test/**/*.spec.js'
+	}
+
+	if (isAbsolute(userDefined)) {
+		userDefined = userDefined.replace(`${join(__dirname)}${sep}`, '')
+	}
+	return userDefined
+}
+
 const { configure } = require('japa')
 configure({
-	files: ['test/**/*.spec.ts'],
+	files: [getTestFiles()],
 })
