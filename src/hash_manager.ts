acc6b526137f56f7b37f324513af0f2b6c3fbbd3
Julien Ripouteau <julien@ripouteau.com>
2026-03-20 18:52:37 +0100

    feat: add Symbol.dispose support to hash fake

--- src/hash_manager.ts
+++ src/hash_manager.ts
@@ -104,12 +104,14 @@ export class HashManager<
   /**
    * Enable fake hash drivers to disable actual hashing for testing
    */
-  fake(): void {
+  fake(): { [Symbol.dispose]: () => void } {
     debug('enabling fakes')
 
     if (!this.#fakeHasher) {
       this.#fakeHasher = new Hash(new Fake())
     }
+
+    return { [Symbol.dispose]: () => this.restore() }
   }
 
   /**
--- tests/hash_manager.spec.ts
+++ tests/hash_manager.spec.ts
@@ -123,6 +123,22 @@ test.group('Hash manager', () => {
     assert.notEqual(await manager.use('scrypt').make('hello-world'), 'hello-world')
   })
 
+  test('restore fake hashers on Symbol.dispose', async ({ assert }) => {
+    const manager = new HashManager({
+      default: 'argon',
+      list: {
+        argon: () => new Argon({}),
+      },
+    })
+
+    {
+      using _ = manager.fake()
+      assert.equal(await manager.use('argon').make('hello-world'), 'hello-world')
+    }
+
+    assert.notEqual(await manager.use('argon').make('hello-world'), 'hello-world')
+  })
+
   test('hash text using the default driver', async ({ assert }) => {
     const manager = new HashManager({
       default: 'argon',
